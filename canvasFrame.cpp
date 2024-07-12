15e5fb329e1f8a343ee5821d8fa0475170b65c29
FatmaAtta <fatmamaali@gmail.com>
2024-07-12 23:34:26 +0300

    canvas frames corrected

--- canvasFrame.cpp
+++ canvasFrame.cpp
@@ -11,15 +11,8 @@ int main(){
         mp[num]++;
     }
     for(auto m:mp){
-        if(m.second%4==0){
-            sum+=m.second/4;
-            m.second=m.second/4-1;
-        }
-        if(m.second%2==0){
-            two+=m.second/2;
-            m.second=m.second/2;
-        }
+            sum+=m.second/2;
     }
-    sum+=two/2;
-    cout<<sum;
+    cout<<sum/2;
+
 }
